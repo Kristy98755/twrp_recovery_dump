c15e9bb56221f2e85d209df0a1e2e5737c6034ce
Kristy98755 <I.g.o.r.56@mail.ru>
2026-08-20 22:44:47 +0600

    Initial TWRP device tree for TPS900 (MSM8937)
    - Generated via twrpdtgen from stock recovery.img
    - Prebuilt kernel from stock recovery
    - Updated fstab with full partition list
    - 32-bit ARM (cortex-a53) configuration
    - GitHub Actions workflow for automated builds

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_recovery_dump.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_recovery_dump-user \
+    omni_recovery_dump-userdebug \
+    omni_recovery_dump-eng
+
---
+++ omni_recovery_dump.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_32_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from recovery_dump device
+$(call inherit-product, device/generic/recovery_dump/device.mk)
+
+PRODUCT_DEVICE := recovery_dump
+PRODUCT_NAME := omni_recovery_dump
+PRODUCT_BRAND := generic
+PRODUCT_MODEL := TPS900
+PRODUCT_MANUFACTURER := generic
+
+PRODUCT_GMS_CLIENTID_BASE := android-generic
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC=""
+
+BUILD_FINGERPRINT := 
+
