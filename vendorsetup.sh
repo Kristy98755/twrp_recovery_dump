0e565e61e052c82d52d7758c2ac151399849f871
Kristy98755 <I.g.o.r.56@mail.ru>
2026-08-20 23:19:15 +0600

    Fix: revert to core_64_bit.mk (core_32_bit doesn't exist in Android 12.1)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,2 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-add_lunch_combo omni_recovery_dump-user
-add_lunch_combo omni_recovery_dump-userdebug
-add_lunch_combo omni_recovery_dump-eng
-
+# vendorsetup.sh is obsolete for TWRP 12.1+
+# COMMON_LUNCH_CHOICES in AndroidProducts.mk is used instead
